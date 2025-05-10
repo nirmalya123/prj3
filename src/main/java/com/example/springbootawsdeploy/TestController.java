3f2c9562cc58f0f465834fa2696dc76a00e40705
nirmalya123 <31580771+nirmalya123@users.noreply.github.com>
2025-05-10 18:26:22 +0530

    Update TestController.java

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -1,24 +1,26 @@
 package com.example.springbootawsdeploy;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/") // Root mapping, so endpoints will be directly accessible
 public class TestController {
-    @GetMapping("/data")
+
+    @GetMapping("demo/data")
     public String getData() {
         return "Project-3 data ok.";
     }
 
-    @GetMapping("/message")
+    @GetMapping("demo/message")
     public String getMessage() {
         return "Project-3 message - Hi.";
     }
-    
-    @GetMapping("/health")
-    public String getMessage() {
-        return "ok";
+
+    @GetMapping("health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
     }
 }
